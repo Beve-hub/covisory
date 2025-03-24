4e5b9a0a419778570782b6d5d513cf2fef3a41a5
Beve-hub <victorokeke@gmail.com>
2025-03-24 16:18:36 +0100

    update

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -1,53 +1,40 @@
 import { useState } from "react";
-import ReactFlagsSelect from 'react-flags-select';
 import { MdLockOpen, MdLockOutline } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import "react-country-state-city/dist/react-country-state-city.css";
+import Logo from "../../assets/white.png";
+import countries from "../../utills/data/Countries"
+import Select from "react-select";
 
-interface Errors {
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  userName?: string;
-  password?: string;
-  confirmPassword?: string;
-  country?: string;
-  address?: string;
-}
+
 
 
 const Register = () => {
-  const [formData, setFormData] = useState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      country: '',
-      userName: '',
-      password: '',
-      confirmPassword: ''
-  })
+ 
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
-  const handleCountrySelect = (countryCode: string) => {
-    setFormData({ ...formData, country: countryCode });
-  };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-    }));
-};
 
 const handleSubmit = () => {
   navigate('/dashboard')
 }
+
+const options = countries.map((country) => ({
+  value: country.code,
+  label: (
+    <div className="flex items-center space-x-2">
+      <img src={country.flag} alt="flag" className="w-5 h-5 rounded-full" />
+      <span>{country.name}</span>
+    </div>
+  ),
+}));
   return (
-    <section className="flex items-center justify-center w-screen h-screen p-4 bg-[var(--primary-color)]">
+    <section className="flex items-center justify-center w-screen h-full p-8 bg-[var(--primary-color)]">
       <div>
-      <h2 className="text-2xl font-bold text-center text-gray-700 mb-4">Logo</h2>
+      <div className="flex justify-center my-4">
+      <img src={Logo} alt="" className="w-[15rem]"/>
+      </div>
       <div className="bg-white p-8 rounded-xl shadow-lg max-w-md w-full">
         <h3 className="text-2xl text-center text-[var(--text-black)] font-bold mb-2">Register</h3>
         <p className="text-md text-center text-[var(--gray-color)] mb-6">Create an account to gain access to panel.</p>
@@ -124,7 +111,7 @@ const handleSubmit = () => {
             <label htmlFor="email" className="block text-sm font-medium text-gray-900 mb-1">
               Country
             </label>
-           
+            <Select options={options} placeholder="Select a country" />
 
           </div>
 
@@ -183,7 +170,7 @@ const handleSubmit = () => {
           </div>
           <button
             type="submit"
-            className="w-full rounded-md bg-[var(--primary-color)] px-4 py-2 text-sm font-semibold text-white shadow-md hover:bg-[var(--secondary-color)] focus:ring-2 focus:ring-indigo-400 focus:outline-none"
+            className="w-full rounded-md bg-[var(--primary-color)] mt-4 px-4 py-2 text-sm font-semibold text-white shadow-md hover:bg-[var(--secondary-color)] focus:ring-2 focus:ring-indigo-400 focus:outline-none"
             onClick={handleSubmit}
           >
             Register
@@ -196,7 +183,7 @@ const handleSubmit = () => {
              <p className="text-[var(--text-black)] text-sm">I agree with <span className="text-[var(--logo-color)]">Privacy & Policy, Terms & Condition</span></p>
             </div>
           
-          <p className="text-[var(--text-black)] text-center">Already have an account? <span className="text-[var(--secondary-color)] font-semibold cursor-pointer" onClick={() => navigate('/login')}>Register</span></p>
+          <p className="text-[var(--text-black)] text-center">Already have an account? <span className="text-[var(--secondary-color)] font-semibold cursor-pointer" onClick={() => navigate('/login')}>Login</span></p>
         </form>
       </div>
       </div>
