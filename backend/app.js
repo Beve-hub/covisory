fce287e8ccd78c8d8858187d65f59b500e4c6c56
Beve-hub <victorokeke@gmail.com>
2025-04-07 20:39:02 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,9 @@ app.use(express.json()); // Middleware to parse JSON
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ssl: true,
+  tlsAllowInvalidCertificates: false,
+  serverSelectionTimeoutMS: 10000,
 })
 
 
