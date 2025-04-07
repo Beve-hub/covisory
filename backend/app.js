4160f976b2c7234f0cdef109cf7b96a83e5668a3
Beve-hub <victorokeke@gmail.com>
2025-04-07 20:27:00 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
 
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
