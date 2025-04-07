e2c17877926d41676ff9cc4e678e8fbaabb9270d
Beve-hub <victorokeke@gmail.com>
2025-04-07 20:52:07 +0100

    pure

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,8 @@ app.use(express.json()); // Middleware to parse JSON
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ssl: true,
+  serverSelectionTimeoutMS: 10000,
 }).then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
