d4ebdc82ee4d970be41376ef5a1dab04e5244bc2
Beve-hub <victorokeke@gmail.com>
2025-04-20 07:40:15 +0100

    update

--- backend/src/route/verifyToken.js
+++ backend/src/route/verifyToken.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken")
 
 module.exports = function(req, res,next) {
-    const token = req.header['auth-token'];
+    const token = req.headers['auth-token'] || req.headers['authorization'];
     if(!token) return res.status(401).send("Access Denied");
 
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verified;
+        const rawToken = token.startsWith("Bearer ") ? token.split(" ")[1] : token;
+        const verified = jwt.verify(rawToken, process.env.TOKEN_SECRET);
+         req.user = verified;
         next();
     } catch(err){
         res.status(400).send("Invalid token")
