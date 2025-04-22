1b6c02c5a44589799067206846689df01b75e5d9
Beve-hub <victorokeke@gmail.com>
2025-04-22 02:12:15 +0100

    types

--- backend/src/model/InvestmentSchema.js
+++ backend/src/model/InvestmentSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const investmentSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Type.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
